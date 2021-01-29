9d203578be6c085c74f03737762673a0c98aa79e
Cody Snell <codyjsnell@yahoo.com>
2021-01-29 17:47:52 -0600

    stubs up server.js

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+const port = process.env.PORT || 3000
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
 
 
+
+app.listen(port,()=> console.log(`Express is up and running on Port:${port}`))
