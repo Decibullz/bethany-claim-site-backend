942168be09f72d8e54892501952f030f44d1f5ed
Cody Snell <codyjsnell@yahoo.com>
2021-01-29 18:40:42 -0600

    adds routes

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-
+const {postItem,deleteItem,getAllItems}= require('./routes/items')
+const {userLogin, userSignup} = require('./routes/user')
 // require the DB
 require(`./database/database`)
 
+// routes
+app.post('/item', postItem)
+app.delete('/item/:itemId', deleteItem)
+app.get('/', getAllItems)
+
+app.post('./signup', userSignup)
+app.post('/login', userLogin)
 
 app.listen(port,()=> console.log(`Express is up and running on Port:${port}`))
