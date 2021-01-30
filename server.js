832c90b42e913f6116192f3b48a958542cba9250
Cody Snell <codyjsnell@yahoo.com>
2021-01-29 18:15:15 -0600

    requires database

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 
 const app = express()
 
+// require the DB
+require(`./database/database`)
 
 
 app.listen(port,()=> console.log(`Express is up and running on Port:${port}`))
