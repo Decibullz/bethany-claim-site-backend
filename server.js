5c9d08fb58379511322313efdf42ba95ed6c1db4
Cody Snell <codyjsnell@yahoo.com>
2021-01-29 20:56:20 -0600

    adds routes and express.json

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const port = process.env.PORT || 3000
 const express = require('express')
 const morgan = require('morgan')
+const methodOverride = require('method-override')
 
 const app = express()
-const {postItem,deleteItem,getAllItems}= require('./routes/items')
-const {userLogin, userSignup} = require('./routes/user')
+
 // require the DB
 require(`./database/database`)
+require('dotenv').config();
+
+const itemsRouter = require('./routes/items');
+// const usersRouter = require('./routes/user');
+
+app.use(morgan(`dev`))
+app.use(express.static(`public`))
+app.use(express.json());
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended:false}))
 
 // routes
-app.post('/item', postItem)
-app.delete('/item/:itemId', deleteItem)
-app.get('/', getAllItems)
+app.use('/items', itemsRouter)
 
-app.post('./signup', userSignup)
-app.post('/login', userLogin)
+
 
 app.listen(port,()=> console.log(`Express is up and running on Port:${port}`))
